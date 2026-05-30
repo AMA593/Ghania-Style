3147e22f254bb1c2c25adbf53c85685501d4eb9d
AMA593 <142753062+AMA593@users.noreply.github.com>
2026-05-30 03:59:47 +0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from '../../firebase-applet-config.json';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1_yYEM3A8FjMRfTQdsseOzcsvH0uMANw",
+  authDomain: "ghania-12207.firebaseapp.com",
+  projectId: "ghania-12207",
+  storageBucket: "ghania-12207.firebasestorage.app",
+  messagingSenderId: "968403392381",
+  appId: "1:968403392381:web:932fc06706138ddc115e14",
+  measurementId: "G-MECZEF3S4V"
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const db = getFirestore(app);
